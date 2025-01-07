7b139e7f12a3e5a5ceeb0d562953cff8ed8b7e64
Haripreeth Avarur <103349948+HaripreethAvarur@users.noreply.github.com>
2025-01-07 18:02:36 +0530

    Delete maslib directory

--- maslib/messages/base.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Dict
-
-class BaseMessage:
-    messages:List[Dict[str, str]]=[]
-    def __init__(self, role: str, content: str):
-        self.role = role
-        self.content = content
-        BaseMessage.messages.append({"role": role, "content": content})
