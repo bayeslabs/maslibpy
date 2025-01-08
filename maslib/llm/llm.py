c58aed88680a8f6eef7eb6f7223d39fc43bc4506
Sathvika Kopalli <ubuntu@ip-172-31-18-228.ec2.internal>
2025-01-08 07:54:22 +0000

    updated llm

--- maslib/llm/llm.py
+++ maslib/llm/llm.py
@@ -62,7 +62,6 @@ class LLM():
             logging.error("Input must be a string or a list of dictionaries for messages.")
             raise ValueError("Input must be a string or a list of dictionaries for messages.")
         try:
-            
             response = completion(model=self.model_name, messages=formatted_messages,stream=False)
             res= response["choices"][0]["message"]["content"]
             ai_msg=AIMessage(content=res)
